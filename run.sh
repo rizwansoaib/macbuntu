6e391985ca7ebc0dbee0fb2d985edacfc80e8587
RIZWAN AHMAD <rizwansoaib@gmail.com>
2019-11-21 21:47:27 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
+sudo apt update -y
+sudo apt upgrade -y
 sudo apt install gnome-tweaks -y
 sudo apt install gnome-shell-extensions -y
 sudo add-apt-repository ppa:indicator-multiload/stable-daily -y
